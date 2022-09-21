17c084ae0ed6894a3d37535e438b3e0c08a6fdea
Alcemir Mocelin <alcemirmjunior@gmail.com>
2022-09-20 23:54:50 -0300

    adição de spring-cloud dependency, criação do modulo eureka-server. configuração do eureka-server e eureka-client para customer e fraud. refatorações necessárias.

--- fraud/src/main/java/alcemirjunior/github/fraud/FraudApplication.java
+++ fraud/src/main/java/alcemirjunior/github/fraud/FraudApplication.java
@@ -2,9 +2,12 @@ package alcemirjunior.github.fraud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FraudApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(FraudApplication.class, args);
     }
